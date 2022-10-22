d198d1879ff43764c1f722604b44386061d65005
L0VELY GUPTA <108621050+Lvlygupta@users.noreply.github.com>
2022-10-22 13:18:16 +0530

    Update Lovely_day2.cpp
    missed an edge case before , now it gets updated

--- Day-2/Lovely_day2.cpp
+++ Day-2/Lovely_day2.cpp
@@ -3,33 +3,26 @@
 using namespace std;
 int main()
 {
-    int n;     //size of the array
+    int n;
     cin >> n;
-
-    int arr[n];     
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-
-    int ans = 1;    // would store the max subarray length
-    int len;        // would store the current subarray length
-    
+    sort(arr, arr + n);
+    int ans = 1;
+    int len;
     for (int i = 0; i < n; i++)
     {
-        int end = i + 1;
-        
-        while (abs(arr[i] - arr[end]) <= 1)
+        int end = n - 1;
+        while (abs(arr[i] - arr[end]) > 1)
         {
-            end++;
+            end--;
         }
-        
-        len = end - i;
+        len = end + 1 - i;
         ans = max(len, ans);
     }
-    
     cout << ans;
     return 0;
 }
-
-// TIME COMPLEXITY : O(n*n)
