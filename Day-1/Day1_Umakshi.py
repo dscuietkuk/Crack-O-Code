d8217968fe8a910f50d68cc1cdbe3ecb72f051cc
Umakshi Sharma <66000862+Umakshi12@users.noreply.github.com>
2022-10-24 16:49:21 +0530

    Update Day1_Umakshi.py
    Here is the correct answer of the Day 1 problem of Hacktoberfest 2k22 week.

--- Day-1/Day1_Umakshi.py
+++ Day-1/Day1_Umakshi.py
@@ -5,7 +5,7 @@ c =0
 def divisibleSumPass(k, arr):
     global i, j, c
     for i in range(len(arr)):
-        for j in range(i + 1, n-1):
+        for j in range(i + 1, n):
             s = arr[i] + arr[j]
             if s % k == 0:
                 c = c + 1
