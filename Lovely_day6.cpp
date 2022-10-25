b7977d120eb02a9c92cf45053c7620e0ef454536
L0VELY GUPTA <108621050+Lvlygupta@users.noreply.github.com>
2022-10-25 22:07:10 +0530

    Create Lovely_day6.cpp

---
+++ Lovely_day6.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+#include<math.h>
+using namespace std;
+
+vector<char> encryption(vector<char> s){
+    vector<char> res;
+    int len = s.size();
+    int n = ceil(sqrt(len));
+    vector<bool> visited(len,false);
+    for(int i = 0;i<len;i++){
+        if(visited[i]==true){
+            continue;
+        }
+        res.push_back(s[i]);
+        for(int j = i+n;j<len;j+=n){
+            res.push_back(s[j]);
+            visited[j] = true;
+        }
+        res.push_back(' ');
+    }
+    return res;
+}
+
+int main(){
+
+string s;
+getline(cin,s);
+vector<char> v;
+for(int i = 0;i<s.size();i++){
+    if(s[i]==' '){
+        continue;
+    }
+    else{
+        v.push_back(s[i]);
+    }
+}
+vector<char> res = encryption(v);
+for(int i = 0;i<res.size();i++){
+    cout<<res[i];
+}
+return 0;}
