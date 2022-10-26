e975c493eccb3c4d743d5fb088ec98a34c9cc2ab
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2022-10-26 15:06:00 +0530

    Solution for Day - 6

---
+++ Day-6/day6.py
@@ -0,0 +1,43 @@
+import math
+string = input()
+
+temp = 0
+string1 = ""
+for x in string.split():
+    temp = temp + len(x)
+    string1 = string1 + x
+
+length = temp
+temp = math.sqrt(temp)
+floor = math.floor(temp)
+ceil = math.ceil(temp)
+
+row = floor #no. of char in the line
+col = ceil #next line
+
+if (floor<=row<=col<=ceil and row*col>=temp):
+    start = 0; end = col; string2=""; final = ""
+    for x in range(0, col):
+        string2 = string2 + string1[start:end] + "\n"
+        start = start + col;
+        end = end + col;
+    L = string2.split("\n")
+    check1 = L.pop()
+    count = 0;
+    val = 0
+    while(count<length):
+        for x in L:
+            try:
+                final = final + x[val]
+                count = count + 1
+            except:
+                continue;
+        val = val + 1
+        if (val%len(L[0]) == 0):
+            val=0
+        final = final + " "
+
+    print(final.rstrip())# By - Hiten Samalia
+else:
+    print(False)
+
